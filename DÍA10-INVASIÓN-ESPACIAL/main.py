6f0d9243fd735ef9070293b1f0f008e258684a83
Jesus Bejar <israelbejarii@gmail.com>
2024-07-02 14:31:27 -0600

    agregue comentarios

--- DÍA10-INVASIÓN-ESPACIAL/main.py
+++ DÍA10-INVASIÓN-ESPACIAL/main.py
@@ -1,11 +1,17 @@
 import pygame
 
+# PARA INICIALIZAR PYGAME
 pygame.init()
 
+# PARA CREAR LA PANTALLA
 # dentro de .set_mode determinas ancho y alto
 # primer numero = alto, segundo numero = ancho
 pantalla = pygame.display.set_mode((800, 600))
 
+#TITULO & ICONO
+
+
+# LOOP DEL JUEGO
 se_ejecuta = True 
 while se_ejecuta:
     for evento in pygame.event.get():
