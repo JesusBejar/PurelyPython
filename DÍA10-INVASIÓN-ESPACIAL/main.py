685660c36b9b7f64cb7add1017fcf839893cb0f0
Jesus Bejar <israelbejarii@gmail.com>
2024-07-02 14:27:37 -0600

    escribí syntaxis para inicializar el juego

--- DÍA10-INVASIÓN-ESPACIAL/main.py
+++ DÍA10-INVASIÓN-ESPACIAL/main.py
@@ -2,4 +2,12 @@ import pygame
 
 pygame.init()
 
+# dentro de .set_mode determinas ancho y alto
+# primer numero = alto, segundo numero = ancho
+pantalla = pygame.display.set_mode((800, 600))
 
+se_ejecuta = True 
+while se_ejecuta:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            se_ejecuta = False
