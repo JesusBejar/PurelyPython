e8929d2cad2077857d919f7f8c0657b571b3ddb6
Jesus Bejar <israelbejarii@gmail.com>
2024-08-16 12:58:11 -0600

    agregue fondo espacial y cambie movimiento

--- DÍA10-INVASIÓN-ESPACIAL/main.py
+++ DÍA10-INVASIÓN-ESPACIAL/main.py
@@ -1,5 +1,6 @@
 from curses import KEY_LEFT
 import pygame
+import random 
 
 # PARA INICIALIZAR PYGAME
 pygame.init()
@@ -8,6 +9,7 @@ pygame.init()
 # dentro de .set_mode determinas ancho y alto
 # primer numero = alto, segundo numero = ancho
 pantalla = pygame.display.set_mode((800, 600))
+fondo = pygame.image.load("./DÍA10-INVASIÓN-ESPACIAL/fondo.png")
 
 #TITULO & ICONO
 pygame.display.set_caption("Invasión Espacial")
@@ -18,14 +20,15 @@ pygame.display.set_icon(icono)
 img_jugador = pygame.image.load("./DÍA10-INVASIÓN-ESPACIAL/cohete.png")
 # variable del jugador
 jugador_x = 368
-jugador_y = 536
+jugador_y = 510
 jugador_x_cambio = 0
 
 # variable del enemigo
 img_enemigo = pygame.image.load("./DÍA10-INVASIÓN-ESPACIAL/enemigo64.png")
-enemigo_x = 368
-enemigo_y = 200
-enemigo_x_cambio = 0
+enemigo_x = random.randint(0, 736)
+enemigo_y = random.randint(50, 200)
+enemigo_x_cambio = 0.5
+enemigo_y_cambio = 50
 
 # funcion jugador 
 def jugador(x, y):
@@ -38,9 +41,11 @@ def enemigo(x, y):
 # LOOP DEL JUEGO
 se_ejecuta = True 
 while se_ejecuta:
+    # imagen de fondo
+    pantalla.blit(fondo, (0, 0))
     # RGB COLOR DE FONDO
-    pantalla.fill((250, 242, 7))
-    # jugador_x += 0.1
+    # pantalla.fill((250, 242, 7))
+    # jugador_x += 0.10
      
     #  iterar eventos
     for evento in pygame.event.get():
@@ -56,7 +61,7 @@ while se_ejecuta:
                 jugador_x_cambio = -0.5
             if evento.key == pygame.K_RIGHT:
                 # print("flecha derecha")
-                jugador_x_cambio = 0.5
+                jugador_x_cambio = .05
         # evento soltar flechas
         # KEYUP
         if evento.type == pygame.KEYUP:
@@ -69,9 +74,21 @@ while se_ejecuta:
     # mantener dentro de bordes
     if jugador_x <= 0:
         jugador_x = 0
-    
     elif jugador_x >= 736:
         jugador_x = 736
+
+    # modificar lugar del enemigo
+    enemigo_x += enemigo_x_cambio
+
+    # mantener dentro de bordes
+    if enemigo_x <= 0:
+        enemigo_x_cambio = .05
+        enemigo_y += enemigo_y_cambio
+        enemigo_y
+    elif enemigo_x >= 736:
+        enemigo_x_cambio = -.05
+        enemigo_y += enemigo_y_cambio
+
 
     jugador(jugador_x, jugador_y)
     enemigo(enemigo_x, enemigo_y)
