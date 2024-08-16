56857f1d28d62afc2b51104e8ffde160b7217839
Jesus Bejar <israelbejarii@gmail.com>
2024-08-16 12:14:12 -0600

    hice las funciones y agregue comentarios

--- DÍA10-INVASIÓN-ESPACIAL/main.py
+++ DÍA10-INVASIÓN-ESPACIAL/main.py
@@ -1,3 +1,4 @@
+from curses import KEY_LEFT
 import pygame
 
 # PARA INICIALIZAR PYGAME
@@ -15,20 +16,46 @@ pygame.display.set_icon(icono)
 
 # ICONO DE JUGADOR
 img_jugador = pygame.image.load("./DÍA10-INVASIÓN-ESPACIAL/cohete.png")
+# variable del jugador
 jugador_x = 368
 jugador_y = 536
+jugador_x_cambio = 0
 
-def jugador():
-    pantalla.blit(img_jugador, (jugador_x, jugador_y))
+# funcion jugador 
+def jugador(x, y):
+    pantalla.blit(img_jugador, (x, y))
 # LOOP DEL JUEGO
 se_ejecuta = True 
 while se_ejecuta:
     # RGB COLOR DE FONDO
     pantalla.fill((250, 242, 7))
+    # jugador_x += 0.1
+     
+    #  iterar eventos
     for evento in pygame.event.get():
+        # evento cerrar
         if evento.type == pygame.QUIT:
             se_ejecuta = False
 
-    jugador()
+        # evento presionar flechas
+        # KEYDOWN
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_LEFT:
+                # print("flecha izquierda")
+                jugador_x_cambio = -0.5
+            if evento.key == pygame.K_RIGHT:
+                # print("flecha derecha")
+                jugador_x_cambio = 0.5
+        # evento soltar flechas
+        # KEYUP
+        if evento.type == pygame.KEYUP:
+            if evento.key == pygame.K_LEFT or evento.key == pygame.K_RIGHT:
+                # print("flecha fue soltada")
+                jugador_x_cambio = 0
+    # modificar lugar del jugador
+    jugador_x += jugador_x_cambio
 
+    jugador(jugador_x, jugador_y)
+
+    # actualizar
     pygame.display.update()
